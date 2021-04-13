f96010ac9f6dcd58c22ef81f4a20099c62cd55b6
safetyh546@gmail.com <safetyh546@gmail.com>
2021-04-13 17:24:27 -0400

    finished all but bonus

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # plotly-challenge
-plotly-challenge
+
+## BellyButton Dashboard
+-Published at Github here: https://safetyh546.github.io/plotly-challenge/<br />
+-An interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.<br />
+
+
+## Main files
+1) Index.html - main html that calls the javascript and data .js files<br />
+2) static\js\app.js - javascript code used generate plotly charts and populate informational table<br />
+3) statis\js\samples.json - belly button data JSON<br />
---
+++ static/js/app.js
@@ -0,0 +1,137 @@
+//build caller function that calls all the pieces (demograph info, charts)
+function optionChanged(SubjectID) {
+    demographInfo(SubjectID)
+    BuildBarAndBubble(SubjectID)
+    // barPlot(person)
+    // buildChart(person)
+  }
+
+//load drop down list
+//call optionchanged function for first ID to populate on initial page load
+d3.json("samples.json").then((data) =>{ 
+    console.log('Test 1')
+    console.log(data)
+
+    //var names = unpack(data.names.data, 0);
+    var names1 = data.names;
+
+    console.log(names1)
+
+    var dropdown = d3.select("#selDataset");
+
+
+    //loop through names and add to dropdown
+    names1.forEach((item) => 
+    {
+        //append dropdown
+        dropdown.append("option").text(item);
+    });
+
+    optionChanged("940")
+
+});
+
+//function to load the demograph info box
+function demographInfo(SubjectID) {
+        d3.json("samples.json").then((data) => {
+
+        var inputValueData = data.samples.filter(item => item.id === SubjectID);
+        console.log(inputValueData);
+
+        //----------------------------------------------------------------------
+        //select the demographic box
+        var Meta = d3.select("#sample-metadata");
+
+        // clear the table 
+        Meta.text("");
+        
+        console.log('right before demograph table')
+        //pull just the meta Data from the full data set (sampleData)
+        var demographTable = data.metadata.filter(item => item.id === parseInt(SubjectID));
+        console.log(demographTable)
+
+        // Append paragraph tags to the demographic info box
+        Meta.append('p').text(`id: ${demographTable[0].id}`);
+        Meta.append('p').text(`ethnicity: ${demographTable[0].ethnicity}`);
+        Meta.append('p').text(`gender: ${demographTable[0].gender}`);
+        Meta.append('p').text(`age: ${demographTable[0].age}`);
+        Meta.append('p').text(`location: ${demographTable[0].location}`);
+        Meta.append('p').text(`bbtype: ${demographTable[0].bbtype}`);
+        Meta.append('p').text(`wfreq: ${demographTable[0].wfreq}`);
+    });
+}
+
+
+//function build bubble and bar chart
+function BuildBarAndBubble(SubjectID) {
+        d3.json("samples.json").then((data) => {
+
+        var inputValueData = data.samples.filter(item => item.id === SubjectID);
+        console.log(inputValueData);
+          //create lists for the first 10 of selected subject ID
+            var top10_SubjectIDs = [];
+            inputValueData[0].otu_ids.slice(0,10).forEach(item => {
+                var substring = top10_SubjectIDs.push(`OTU ${item}`)
+            });
+    
+            console.log(top10_SubjectIDs)
+    
+            var top10_sampleValues = inputValueData[0].sample_values.slice(0,10);
+            console.log(top10_sampleValues)
+            
+            var top10_OTULabels = inputValueData[0].otu_labels.slice(0,10);
+            console.log(top10_OTULabels )
+         
+         //--------------------------------------------------------------------
+         //bar chart
+          var BarChartData =
+         [{
+             type: 'bar',
+             x: top10_sampleValues,
+             y: top10_SubjectIDs,
+             orientation: 'h',
+             text: top10_OTULabels,
+             mode: 'markers'
+         }]
+    
+         var BarChartLayout =
+         { yaxis:{autorange: 'reversed'}};
+    
+         Plotly.newPlot('bar',BarChartData, BarChartLayout)
+
+        //-------------------------------------------------------------------
+        //bubble chart
+        
+        //list all otu_IDs along x-axis
+        var xaxis = inputValueData[0].otu_ids;
+        console.log(xaxis);
+
+        var bubbleChartData = 
+            [{
+                x: xaxis,
+                y: top10_sampleValues,
+                mode: 'markers',
+                text: top10_OTULabels,
+                marker: 
+                {
+                size: top10_sampleValues,
+                color: xaxis,
+                colorscale: [[0, 'rgb(51, 70, 255)'], [1, 'rgb(66, 255, 51)']]  //0 color is bluish, 1 color is green from https://htmlcolorcodes.com/
+                }
+            }];
+            
+            var bubbleChartLayout = 
+            {
+                showlegend: false,
+                height: 600,
+                width: 1200,
+                xaxis: {title: 'OTU ID'},
+                opacity: 0.6
+            };
+            
+            Plotly.newPlot('bubble',bubbleChartData, bubbleChartLayout);
+
+        });
+
+}
+ 
